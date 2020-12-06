a52a558465509ac0a022cacc68b1e8ae07491ee4
Stas Smirnov <smistav@ya.ru>
2020-12-06 14:24:59 +0300

    add begin card to js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,71 @@
-const handleEditButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const handlePopupClose = document.querySelector('.popup__close');
-let inputPopupName = document.querySelector('.popup__input[name="name"');
-let inputPopupJob = document.querySelector('.popup__input[name="job-do"]');
-let profileName = document.querySelector('.profile__name');
-let profilePersonDo = document.querySelector('.profile__person-do');
-let formElement = document.querySelector('.popup__container');
+const InitialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+const HandleEditButton = document.querySelector('.profile__edit-button');
+const Popup = document.querySelector('.popup');
+const HandlePopupClose = document.querySelector('.popup__close');
+let InputPopupName = document.querySelector('.popup__input[name="name"');
+let InputPopupJob = document.querySelector('.popup__input[name="job-do"]');
+let ProfileName = document.querySelector('.profile__name');
+let ProfilePersonDo = document.querySelector('.profile__person-do');
+let FormElement = document.querySelector('.popup__container');
 
-function showPopup() {
-  inputPopupName.value = profileName.textContent;
-  inputPopupJob.value = profilePersonDo.textContent;
-  popup.classList.add('popup_opened');
+const CardTemplate = document.querySelector('#elem').content;
+const ElementsOnlineItem = document.querySelector('.elements');
+
+// функция добавления карточки
+function AddItem(item) {
+  const ElementsItem = CardTemplate.cloneNode(true);
+
+  ElementsItem.querySelector('.elements__img').src = item.link;
+  ElementsItem.querySelector('.elements__heading').textContent = item.name;
+  ElementsOnlineItem.append(ElementsItem);
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+// Вывод дефолтного массива
+InitialCards.forEach(item => AddItem(item));
+
+
+
+function ShowPopup() {
+  InputPopupName.value = ProfileName.textContent;
+  InputPopupJob.value = ProfilePersonDo.textContent;
+  Popup.classList.add('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function ClosePopup() {
+  Popup.classList.remove('popup_opened');
+}
+
+function FormSubmitHandler(evt) {
   evt.preventDefault();
-  profileName.textContent = inputPopupName.value;
-  profilePersonDo.textContent = inputPopupJob.value;
-  closePopup();
+  ProfileName.textContent = InputPopupName.value;
+  ProfilePersonDo.textContent = InputPopupJob.value;
+  ClosePopup();
 }
-handleEditButton.addEventListener('click', showPopup);
-handlePopupClose.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+HandleEditButton.addEventListener('click', ShowPopup);
+HandlePopupClose.addEventListener('click', ClosePopup);
+FormElement.addEventListener('submit', FormSubmitHandler);
