df38603e52025901486d72653fb6353ad294e1ee
Stas Smirnov <smistav@ya.ru>
2020-12-07 11:20:59 +0300

    add remove like

--- js/script.js
+++ js/script.js
@@ -31,28 +31,35 @@ let inputPopupNameF = document.querySelector('.popup__input[name="namefirst"');
 let inputPopupNameS = document.querySelector('.popup__input[name="namesecond"]');
 let profileName = document.querySelector('.profile__name');
 let headerPopup = document.querySelector('.popup__heading');
-let popupButton = document.querySelector('.popup__button');
 let profilePersonDo = document.querySelector('.profile__person-do');
 const formElement = document.querySelector('.popup__container');
 const handleAddCardButton = document.querySelector('.profile__add-button');
 const headerNewCardPopup = 'Новое место';
 const headerEditPopup = 'Редактировать профиль';
+
 const cardTemplate = document.querySelector('#elem').content;
 const elementsOnlineItem = document.querySelector('.elements');
+
 
 // функция добавления карточки
 function addItem(item) {
   const elementsItem = cardTemplate.cloneNode(true);
-
+  const handleElementHeart = elementsItem.querySelector('.elements__heart-button');
   elementsItem.querySelector('.elements__img').src = item.link;
   elementsItem.querySelector('.elements__heading').textContent = item.name;
   elementsItem.querySelector('.elements__img').alt = item.name;
   elementsOnlineItem.prepend(elementsItem);
+  handleElementHeart.addEventListener('click', addRemoveLike);
 }
 
 // Вывод дефолтного массива
 initialCards.forEach(item => addItem(item));
 
+function addRemoveLike(event) {
+  let targetItem = event.target;
+  targetItem = targetItem.closest('.elements__heart-button')
+  targetItem.classList.toggle('elements__heart-button_active');
+};
 
 function showEditPopup() {
   headerPopup.textContent = headerEditPopup;
@@ -74,20 +81,26 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function submitFormHandler(evt) {
-  evt.preventDefault();
+function addEdit() {
+  profileName.textContent = inputPopupNameF.value;
+  profilePersonDo.textContent = inputPopupNameS.value;
+}
+function addNewItem() {
   let newCard = [{ name: '', link: '' }];
 
-  if (headerPopup.textContent === headerEditPopup) {
-    profileName.textContent = inputPopupNameF.value;
-    profilePersonDo.textContent = inputPopupNameS.value;
+  if (inputPopupNameF.value !== '' && inputPopupNameS.value !== '') {
+    newCard.name = inputPopupNameF.value;
+    newCard.link = inputPopupNameS.value;
+    addItem(newCard);
   }
+}
+
+function submitFormHandler(evt) {
+  evt.preventDefault();
   if (headerPopup.textContent === headerNewCardPopup) {
-    if (inputPopupNameF.value !== '' && inputPopupNameS.value !== '') {
-      newCard.name = inputPopupNameF.value;
-      newCard.link = inputPopupNameS.value;
-      addItem(newCard);
-    }
+    addNewItem();
+  } if (headerPopup.textContent === headerEditPopup) {
+    addEdit();
   }
   closePopup();
 }
