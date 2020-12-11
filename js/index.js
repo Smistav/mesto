41f29180cb6c5720c27b6b495f2a09956aa559ed
Stas Smirnov <smistav@ya.ru>
2020-12-11 15:37:25 +0300

    add js initial cards and fix element_img

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 // Profile
 const profileName = document.querySelector('.profile__name');
 const profilePersonDo = document.querySelector('.profile__person-do');
@@ -118,6 +92,7 @@ function submitFormHandlerEdit(evt) {
 function submitFormHandlerAdd(evt) {
   evt.preventDefault();
   let newCard = [{ name: '', link: '' }];
+
   if (inputPopupAddPlace.value !== '' && inputPopupAddlink.value !== '') {
     newCard.name = inputPopupAddPlace.value;
     newCard.link = inputPopupAddlink.value;
