150e69307de845338b804d4490a2b874086771a4
Stas Smirnov <smistav@ya.ru>
2020-12-18 16:59:42 +0300

    release esc

--- js/index.js
+++ js/index.js
@@ -1,30 +1,26 @@
-//
-const body = document.querySelector('.page');
-// Key close
-const keyClose = 'Escape';
-// Profile
+// Key close popup
+const ESCAPE = 'Escape';
+// Profile const
 const profileName = document.querySelector('.profile__name');
 const profilePersonDo = document.querySelector('.profile__person-do');
-// Overlay
-const allOverlayPopup = document.querySelectorAll('.popup');
-// elements
+// elements const
 const cardTemplate = document.querySelector('#elem').content;
 const elementsOnlineItem = document.querySelector('.elements');
-// Popup Edit
+// Popup Edit const
 const handleEditButton = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_form_edit');
 const handlePopupEditClose = popupEdit.querySelector('.popup__close');
 const inputPopupEditName = popupEdit.querySelector('.popup__input[name="name"]');
 const inputPopupEditJob = popupEdit.querySelector('.popup__input[name="job"]');
 const formElementEdit = popupEdit.querySelector('.popup__container');
-// Popup Add
+// Popup Add const
 const handleAddButton = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup_form_add');
 const handlePopupAddClose = popupAdd.querySelector('.popup__close');
 const inputPopupAddPlace = popupAdd.querySelector('.popup__input[name="place"]');
 const inputPopupAddlink = popupAdd.querySelector('.popup__input[name="link"]');
 const formElementAdd = popupAdd.querySelector('.popup__container');
-// Popup Img
+// Popup Img const
 const popupImg = document.querySelector('.popup_form_img');
 const handlePopupImgClose = popupImg.querySelector('.popup__close');
 const formElementImg = popupImg.querySelector('.popup__container');
@@ -42,6 +38,7 @@ function addCard(newCardboolean, item) {
   if (newCardboolean) { elementsOnlineItem.prepend(card); }
   else { elementsOnlineItem.append(card); }
 }
+
 function createCard() {
   const elementsItem = cardTemplate.cloneNode(true);
   const handleElementHeart = elementsItem.querySelector('.elements__heart-button');
@@ -98,14 +95,12 @@ function submitFormHandlerEdit(evt) {
 }
 
 function submitFormHandlerAdd(evt) {
-  evt.preventDefault();
   const newCard = [{ name: '', link: '' }];
 
-  if (inputPopupAddPlace.value !== '' && inputPopupAddlink.value !== '') {
-    newCard.name = inputPopupAddPlace.value;
-    newCard.link = inputPopupAddlink.value;
-    addCard(true, newCard);
-  }
+  evt.preventDefault();
+  newCard.name = inputPopupAddPlace.value;
+  newCard.link = inputPopupAddlink.value;
+  addCard(true, newCard);
   closePopup(popupAdd);
 }
 
@@ -114,29 +109,28 @@ function submitFormHandlerImg(evt) {
   closePopup(popupImg);
 }
 
-function closeKeyPopup(evt, popup) {
-  if (evt.key === keyClose) closePopup(popup);
+function closeEscPopup(evt) {
+  const activePopup = document.querySelector('.popup_opened');
+
+  if (evt.key === ESCAPE) closePopup(activePopup);
+}
+
+function closeClickOverlay(evt) {
+  const activePopup = document.querySelector('.popup_opened');
+
+  if (evt.target === activePopup) closePopup(activePopup);
 }
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
-  body.addEventListener('keydown', evt => closeKeyPopup(evt, popup));
+  document.addEventListener('keydown', closeEscPopup);
+  document.addEventListener('click', closeClickOverlay);
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
-  body.removeEventListener('keydown', evt => closeKeyPopup(evt, popup));
-}
-
-function setListenersCloseOnOverlay(allPopup) {
-  const arrPopup = Array.from(allPopup);
-  arrPopup.forEach((popup) => {
-    popup.addEventListener('click', (evt) => {
-      if (evt.target === popup) {
-        closePopup(evt.target);
-      }
-    });
-  });
+  document.removeEventListener('keydown', closeEscPopup);
+  document.removeEventListener('click', closeClickOverlay);
 }
 
 // слушатели PopupEdit
@@ -150,5 +144,3 @@ formElementAdd.addEventListener('submit', submitFormHandlerAdd);
 // слушатели PopupImg
 handlePopupImgClose.addEventListener('click', () => { closePopup(popupImg) });
 formElementImg.addEventListener('submit', submitFormHandlerImg);
-// слушатель на Overlay всех popup
-setListenersCloseOnOverlay(allOverlayPopup);
