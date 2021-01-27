40ef4e97381a7d6fe1533a7e2549f9523fa62dd8
Stas Smirnov <smistav@ya.ru>
2021-01-27 12:25:21 +0300

    popupWithImage release

--- components/Popup.js
+++ components/Popup.js
@@ -1,23 +1,24 @@
+import { ESCAPE } from '../components/costants.js'
 export default class Popup {
   constructor(popupSelector) {
     this._popupSelector = document.querySelector(popupSelector);
   }
   _handleEscClose(evt) {
-    if (evt.key === 'Escape') this.closePopup(this._popupSelector);
+    if (evt.key === ESCAPE) this.close(this._popupSelector);
   }
   _handleOverlayClose(evt) {
-    if (evt.target === this._popupSelector) this.closePopup(this._popupSelector);
+    if (evt.target === this._popupSelector) this.close(this._popupSelector);
   }
   setEventListeners() {
     const buttonClosePopup = this._popupSelector.querySelector('.popup__close');
-    buttonClosePopup.addEventListener('click', () => this.closePopup());
+    buttonClosePopup.addEventListener('click', () => this.close());
   }
-  openPopup() {
+  open() {
     this._popupSelector.classList.add('popup_opened');
     document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
     document.addEventListener('click', (evt) => this._handleOverlayClose(evt));
   }
-  closePopup() {
+  close() {
     this._popupSelector.classList.remove('popup_opened');
     document.removeEventListener('keydown', (evt) => this._handleEscClose(evt));
     document.removeEventListener('click', (evt) => this._handleOverlayClose(evt));
